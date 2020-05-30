d58a6a86525764bd019649eb760f237477cc7f11
Sirwan <sir1afifi@gmail.com>
2020-05-30 23:33:49 +0430

    Add fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    Vazir: require("./app/assets/fonts/Vazir.ttf"),
+    "Vazir-Bold": require("./app/assets/fonts/Vazir-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return <View></View>;
 }
 
